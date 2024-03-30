823a3c4ecd50ea153e2c874226a01aeca1e2363e
platinumazure <platinumazure@gmail.com>
2024-03-30 10:56:26 -0500

    Add basic TTS script

---
+++ tts.ps1
@@ -0,0 +1,8 @@
+Add-Type -AssemblyName System.Speech
+$speechSynthesizer = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+
+$speechSynthesizer.SelectVoice('Microsoft Susan')
+$speechSynthesizer.Rate = 3;
+
+$speechSynthesizer.Speak('Hello, world!')
+$speechSynthesizer.Speak('What''s up?')
