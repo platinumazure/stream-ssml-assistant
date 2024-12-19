1fde9bdc2f43c8f7db5a5bdaf60dee2cc3f5e56c
platinumazure <platinumazure@gmail.com>
2024-12-18 22:05:32 -0600

    Fix import statement in index.ts to include file extension

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console -- This is a CLI script, so we need to use console.log */
 import process from 'node:process';
-import { ChatMessage } from './chat-message';
+import { ChatMessage } from './chat-message.js';
 
 const username = process.argv[2];
 const chatMessage = process.argv[3];
