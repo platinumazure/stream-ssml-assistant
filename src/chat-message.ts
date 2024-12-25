9acee21d6fa0a8d3bacff026e3e630d7827f7736
platinumazure <platinumazure@gmail.com>
2024-12-25 10:47:50 -0600

    Enhance SSML output in ChatMessage class to include phoneme generation for usernames

--- src/chat-message.ts
+++ src/chat-message.ts
@@ -1,8 +1,30 @@
+const usernamePronunciationMap: Record<string, string> = {
+  platinumazure: '"pl{tIn@m{V@r',
+};
+
+// Regular expression to capture words
+const wordRegex = /\w+/g;
+
 export class ChatMessage {
   constructor(private username: string, private message: string) {
   }
 
+  generatePhoneme(word: string): string {
+    const lowerCaseWord = word.toLowerCase();
+
+    if (lowerCaseWord in usernamePronunciationMap) {
+      const ssmlFriendlyPronunciation = usernamePronunciationMap[lowerCaseWord].replace(/"/g, '&quot;');
+
+      return `<phoneme alphabet="x-sampa" ph="${ssmlFriendlyPronunciation}">${word}</phoneme>`;
+    }
+
+    return word;
+  }
+
   toSSML(): string {
-    return `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en"><p>${this.username} says: ${this.message}</p></speak>`;
+    const usernameToRead = this.generatePhoneme(this.username);
+    const messageToRead = this.message.replace(wordRegex, word => this.generatePhoneme(word));
+
+    return `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en"><p>${usernameToRead} says: ${messageToRead}</p></speak>`;
   }
 }
